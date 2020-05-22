f25d2a58c1cc9a2881eaf861b43966d4ad888289
ShadowGod001 <55005167+ShadowGod001@users.noreply.github.com>
2020-05-22 20:21:23 -0300

    Ej completo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PII_Gull_GRASP_and_SOLID_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
